a39d6237790765c98031c76b0d4e272b14a6e404
d1m0 <dimocracy@gmail.com>
2018-05-22 10:17:18 -0700

    ubuntu16 vagrant kubeadm1.10.3 works

--- vagrant/beatrak-ubuntu16/bootstrap.sh
+++ vagrant/beatrak-ubuntu16/bootstrap.sh
@@ -9,11 +9,15 @@ fi
 echo "----------------------------------------"
 echo "- installing docker"
 echo "----------------------------------------"
-apt-get update
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
-apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
+if [[ $(docker version 2>&1) != *"Version"* ]]; then
+    apt-get update
+    apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+    add-apt-repository "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
+    apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
+    sudo apt-mark hold docker-ce
+fi
+
 
 echo "----------------------------------------"
 echo "- installing go"
@@ -56,19 +60,6 @@ if [[ $(kubectl get pods 2>&1) != *"No resources found."* ]]; then
 	go get github.com/kubernetes-incubator/cri-tools/cmd/crictl
     fi
 
-    # template
-    #curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
-    #	echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
-    #	sudo apt-get update -q && \
-    #	sudo apt-get install -qy kubelet=<version> kubectl=<version> kubeadm=<version>
-
-    # old
-    #    apt-get update && apt-get install -y apt-transport-https curl
-    #    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-    # cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-    # deb http://apt.kubernetes.io/ kubernetes-xenial main
-    # EOF
-
     curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
     	echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
     
@@ -103,43 +94,39 @@ if [[ $(weave 2>&1) = *"command not found"* ]]; then
 ##    weave status
 fi
 
-#echo "----------------------------------------"
-#echo "- jdk"
-#echo "----------------------------------------"
-#apt-get install -y openjdk-8-jdk
-#apt-get install -y maven
+echo "----------------------------------------"
+echo "- jdk"
+echo "----------------------------------------"
+apt-get install -y openjdk-8-jdk
+apt-get install -y maven
 
-#echo "----------------------------------------"
-#echo "- dev tools"
-#echo "----------------------------------------"
-#apt-get install -y build-essential git make
-#apt-get install -y dkms build-essential linux-headers-`uname -r`
-#apt-get install -y virtualbox-guest-utils virtualbox-guest-dkms
-#apt-get install -y libtool
-#apt-get install -y cmake
-#apt-get install -y realpath
-#apt-get install -y clang-format-5.0
-#apt-get install -y automake
+echo "----------------------------------------"
+echo "- dev tools"
+echo "----------------------------------------"
+apt-get install -y build-essential git make
+apt-get install -y dkms build-essential linux-headers-`uname -r`
+apt-get install -y virtualbox-guest-utils virtualbox-guest-dkms
+apt-get install -y libtool
+apt-get install -y cmake
+apt-get install -y realpath
+apt-get install -y clang-format-5.0
+apt-get install -y automake
 
-#echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
-#curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
-#apt-get update && apt-get install -y bazel
-#apt-get upgrade bazel
-#go get github.com/bazelbuild/buildtools/buildifier
+echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
+curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
+apt-get update && apt-get install -y bazel
+apt-get upgrade bazel
+go get github.com/bazelbuild/buildtools/buildifier
 
-#cd /tmp
-#git clone https://github.com/bats-core/bats-core.git
-#cd bats-core
-#./install.sh /usr/local
+cd /tmp
+git clone https://github.com/bats-core/bats-core.git
+cd bats-core
+./install.sh /usr/local
 
-#echo "----------------------------------------"
-#echo "- nodejs tools"
-#echo "----------------------------------------"
-#curl -sL https://deb.nodesource.com/setup_8.x |  bash -
-#apt-get install -y nodejs
-#npm install --global yarn forever
-
-
-
-
-
+echo "----------------------------------------"
+echo "- nodejs tools"
+echo "----------------------------------------"
+curl -sL https://deb.nodesource.com/setup_8.x |  bash -
+apt-get install -y nodejs
+npm install --global yarn forever
+npm update --global
